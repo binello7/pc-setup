94067fd222bb69b623c181636b3eb7a3cd0d5823
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2022-05-12 18:54:50 +0200

    Update packages

--- install/apt-install.bash
+++ install/apt-install.bash
@@ -7,15 +7,6 @@ apt_pkgs=(
   wakeonlan
 )
 
-echo "Add new repositories to sources.list..."
-echo -e '\ndeb https://qgis.org/ubuntu bionic main' | sudo tee -a /etc/apt/sources.list
-sudo add-apt-repository ppa:linuxuprising/shutter
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
-
-echo "Add repositories keys..."
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 51F523511C7028C3
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-
 echo "Update apt-index..."
 sudo apt update
 
